ba5b3f9a1bb2a4aa0719a555101e5f952273a30d
chengzhx76 <chengzhx76@qq.com>
2020-03-12 13:55:22 +0800

    增加JDK源码

---
+++ simple-demo/simple-jdk-demo/src/main/java/com/github/chengzhx76/jdk/lang/StringBufferTest.java
@@ -0,0 +1,13 @@
+package com.github.chengzhx76.jdk.lang;
+
+/**
+ * @Description
+ * @Author admin
+ * @Date 2020/3/12 11:31
+ * @Version 3.0
+ */
+public class StringBufferTest {
+    public static void main(String[] args) {
+        StringBuffer buffer = new StringBuffer();
+    }
+}
---
+++ simple-demo/simple-jdk-demo/src/main/java/com/github/chengzhx76/jdk/lang/StringBuilderTest.java
@@ -0,0 +1,13 @@
+package com.github.chengzhx76.jdk.lang;
+
+/**
+ * @Description
+ * @Author admin
+ * @Date 2020/3/12 11:26
+ * @Version 3.0
+ */
+public class StringBuilderTest {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+    }
+}
