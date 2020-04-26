2df640a92e4538aa1814142b82a280a5e55af255
chengzhx76 <chengzhx76@qq.com>
2020-04-26 23:35:40 +0800

    lean Integer

---
+++ simple-demo/simple-jdk-demo/src/main/java/com/github/chengzhx76/jdk/lang/IntegerTest.java
@@ -0,0 +1,27 @@
+package com.github.chengzhx76.jdk.lang;
+
+/**
+ * Desc:
+ * Author: 光灿
+ * Date: 2020/4/26
+ */
+public class IntegerTest {
+
+
+    public static void main(String[] args) {
+
+        Integer i = Integer.valueOf(1);
+
+        //Integer i1 = new Integer(127);
+        //Integer i2 = Integer.valueOf(127);
+        //Integer i3 = 127;
+        //
+        //Integer i11 = 128;
+        //Integer i12= 128;
+        //
+        //System.out.println(i1 == i2);
+        //System.out.println(i2 == i3);
+        //System.out.println(i11 == i12);
+
+    }
+}
