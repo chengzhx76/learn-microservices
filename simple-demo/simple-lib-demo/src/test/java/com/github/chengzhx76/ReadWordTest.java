40b213f90c98f374ae7a2884eaa72ffd4b14a351
chengzhx76 <chengzhx76@qq.com>
2023-01-12 11:15:25 +0800

    word 解析

--- simple-demo/simple-lib-demo/src/test/java/com/github/chengzhx76/ReadWordTest.java
+++ simple-demo/simple-lib-demo/src/test/java/com/github/chengzhx76/ReadWordTest.java
@@ -25,10 +25,11 @@ public class ReadWordTest {
 
     static final Pattern REX_NUMBER = Pattern.compile("\\d+");
     static final Pattern REX_LETTER = Pattern.compile("[a-zA-Z]");
+    static final Pattern REX_CHINESE = Pattern.compile("[^\\x00-\\xff]");
 
     public static void main(String[] args) throws IOException {
 
-        String fileName = "D:\\a.docx";
+        String fileName = "D:\\b.docx";
 
         InputStream is = Files.newInputStream(Paths.get(fileName));
         try (XWPFDocument doc = new XWPFDocument(is)) {
@@ -45,27 +46,55 @@ public class ReadWordTest {
                 if (StrUtil.isBlank(line)) {
                     continue;
                 }
+                line = StrUtil.trimStart(line);
                 String firstLetter = line.substring(0, 1);
                 if (ReUtil.isMatch(REX_NUMBER, firstLetter)) {
+
                     question = line;
                     number = ReUtil.getGroup0(REX_NUMBER, question);
-                } else if (ReUtil.isMatch(REX_LETTER, firstLetter)) {
-                    options.add(line);
+                } else if ((ReUtil.isMatch(REX_LETTER, firstLetter)
+                        || ReUtil.isMatch(REX_CHINESE, firstLetter))
+                        && !StrUtil.equals(firstLetter, "答")) {
+
+                    String[] answers = line.split("[a-zA-Z]");
+                    for (int i = 0; i <answers.length ; i++) {
+                        if (!StrUtil.isBlank(answers[i])) {
+                            String _answer = answers[i];
+                            String prefix = "";
+                            if (i == 0) {
+                                prefix = "A";
+                            } else if (i == 1) {
+                                prefix = "B";
+                            } else if (i == 2) {
+                                prefix = "C";
+                            } else if (i == 3) {
+                                prefix = "D";
+                            }
+                            _answer = StrUtil.trim(_answer);
+                            options.add(prefix + _answer);
+                        }
+                    }
+
                 } else if (StrUtil.equals(firstLetter, "答")) {
                     isEnd = true;
                     answer = ReUtil.get(REX_LETTER, line, 0);
                 } else {
-                    System.err.println("未知 " + line);
+                    System.err.println("未知 line[" + number + "] " + line);
                 }
 
                 if (isEnd) {
 
+
                     System.out.println("编号：" + number);
                     System.out.println("题目：" + question);
-                    System.out.println("选项：" + options);
+                    System.out.println("选项：len[" + options.size() + "]" + options);
                     System.out.println("答案：" + answer);
                     System.out.println("-------------------");
 
+                    if (options.size() > 4) {
+                        System.err.println("编号：" + number);
+                        return;
+                    }
                     isEnd = false;
                     question = "";
                     number = "";
